c83957fc2f663a5ac0f1fbf2de6dd09e6098a39c
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2021-07-22 04:49:21 -0700

    ey ey ey, working on the gui a lot

--- GUI/javascript/navigation.js
+++ GUI/javascript/navigation.js
@@ -2,6 +2,8 @@
 
 let navigation = document.getElementsByClassName("navop");
 
+let menunavigation = document.getElementsByClassName("menunav");
+
 for(let nav_button of navigation){
 	nav_button.addEventListener("click", function(){
 		let get_to = this.getAttribute("goto");
@@ -14,4 +16,18 @@ for(let nav_button of navigation){
 		page.style.zIndex = 1;
  	})
 }
+
+for(let menu_button of menunavigation){
+	menu_button.addEventListener("click", function(){
+		let get_to = this.getAttribute("goto");
+		let page = document.getElementById(get_to);
+		let pages = document.getElementsByClassName("container");
+
+		for(article of pages){
+			article.style.zIndex = 0;
+		}
+		page.style.zIndex = 1;
+ 	})
+}
+
 
