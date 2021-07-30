8bd05140046d82232fa6db23fc747174accb98cc
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2021-07-30 03:34:16 -0700

    input password, decrypt, clean and copy

--- GUI/javascript/getaccounts.js
+++ GUI/javascript/getaccounts.js
@@ -1,14 +1,40 @@
 
 let account_container = document.getElementById("account-container");
-
-
+let ultimate_password = document.getElementById("confirmpass");
+let universalpass = document.getElementById("universalpass");
 
 function get_account_information(){
-	//Todo
-	console.log(this.getElementsByClassName("infotag")[2].getAttribute('hashedpass'));
+	ultimate_password.style.zIndex = 4;
+	let hash = this.getElementsByClassName("infotag")[2].getAttribute("hashedpass")
+	universalpass.setAttribute("hashedpass", hash);
 }
 
 
+
+
+//Check if password is correct:
+universalpass.addEventListener("input", function(){
+	check_if_password_correct(this.value)
+	.then( r => {
+		if(r){
+			eel.decrypt(this.value, this.getAttribute("hashedpass"));
+			eel.notify("Correct password");
+			ultimate_password.style.zIndex = 0;
+			this.value = "";
+			this.setAttribute("hashedpass", "");
+		}
+	});
+});
+
+
+async function check_if_password_correct(password){
+	let correct = await eel.check_password(password)();
+	return correct;
+}
+
+
+
+//Populate feed with accounts.
 function populate(account_list){
 	account_container.innerHTML = '';
 	for(account of account_list){
@@ -63,15 +89,17 @@ get_accounts()
 //Search accounts.
 let search_bar = document.getElementById("searcbarinput");
 
-
+//Change the feed everytime that the user types in the searchbar.
 search_bar.addEventListener("input", function(){
 	let current_input = search_bar.value;
 	let search_options = [];
 	for(account of ACCOUNTS){
-		console.log(account)
+		//Check if what the person is typing matches to the site or user.
 		if(account['site'].indexOf(current_input) != -1 || account['user'].indexOf(current_input) != -1){
 			search_options.push(account);
 		}
 	}
+
+	//Populate the feed with all the matching options
 	populate(search_options);
 });
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from lib.data_management import *
 from lib.data_generator import *
 from lib.cryptography import *
 from codecs import open as copen
-
+import pyperclip
 
 TOOLS = {
 	"DATAGEN": DataGenerator(),
@@ -14,10 +14,33 @@ TOOLS = {
 	"CRYPTO": CipherHandler()
 }
 
+
+
+@eel.expose
+def notify(message):
+	#TODO: Toast notification.
+	print(message)
 	
+
+@eel.expose()
+def decrypt(key, hashed_pass, copy_to_clipboard=True):
+	result = TOOLS['CRYPTO'].decrypt(hashed_pass, key)
+
+	if copy_to_clipboard:
+		pyperclip.copy(result)
+
+	return result
+
+
+@eel.expose
+def copy(text):
+	pyperclip.copy(text)
 
 @eel.expose
 def get_accounts():
+	'''
+	return list of account information, containing site, username and hashed password.
+	'''
 	accounts = []
 
 	for site in TOOLS['STORAGE'].data['sites']:
@@ -31,20 +54,20 @@ def get_accounts():
 	return accounts
 
 
+
+@eel.expose
+def check_password(password):
+	return TOOLS['CRYPTO'].checksum(password, TOOLS['STORAGE'].data['checksum'])
+
+
 def on_close(page, sockets):
 	print(page, 'closed')
 	print('Still have sockets open to', sockets)
 
 def main():
-	web_options = {
-		"mode": "chrome-app",
-		"host": "localhost",
-		"port": 8000
-	}
 	eel.init("GUI")
-
 	eel.start("index.html",  size=(1216, 739), callback=on_close)
-	#storage = Storage()
+
 
 
 if __name__ == '__main__':
